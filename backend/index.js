ccae7a3b6bb80babdf5f1f888e026995213453c2
imlnr <imlnr2002@gmail.com>
2024-02-21 01:36:49 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
 const cors = require('cors');
+const { connection } = require('./config/db');
 require('dotenv').config();
 const app = express();
 
@@ -7,10 +8,12 @@ const port = process.env.port || 4500;
 
 
 
-app.listen(port,()=>{
+app.listen(port,async()=>{
     try {
+        await connection;
+        console.log('Connected to db....');
         console.log(`Your server is Running at port ${port}`);
     } catch (error) {
-        
+        console.log(error);
     }
 })
