c88309e0711dcdc9c0d7b0a737e1baa5be113b62
imlnr <imlnr2002@gmail.com>
2024-02-21 13:08:26 +0530

    final commit

--- frontend/my-react-app/src/AllRoutes/MainRoutes.jsx
+++ frontend/my-react-app/src/AllRoutes/MainRoutes.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from '../Pages/Login'
+import Signup from '../Pages/Signup'
 
 const MainRoutes = () => {
   return (
-    <div>MainRoutes</div>
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+    </Routes>
   )
 }
 
