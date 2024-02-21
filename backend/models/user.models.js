3b3d3624cc67f3454057b67b7df0b191e52f1759
imlnr <imlnr2002@gmail.com>
2024-02-22 02:38:03 +0530

    final commit

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    Name: { type: String, required: true },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     age: { type: Number, required: true },
     password: { type: String, required: true },
