25b49d77c4ccdd6486a73e8fcbf481bb67209708
imlnr <imlnr2002@gmail.com>
2024-02-21 16:37:12 +0530

    final commit

--- backend/models/pets.model.js
+++ backend/models/pets.model.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 
 const petSchema = mongoose.Schema({
-    petname:{type:String,required:true},
-    breed:{type:String,required:true},
-    gender:{type:String,required:true},
-    price:{type:Number,required:true}
-},{
-    versionKey:false
+    petname: { type: String, required: true },
+    breed: { type: String, required: true },
+    gender: { type: String, required: true },
+    price: { type: Number, required: true },
+    age: { type: Number, required: true },
+    about: { type: String, required: true }
+}, {
+    versionKey: false
 })
 
-const PetModel = mongoose.model('pets',petSchema);
+const PetModel = mongoose.model('pets', petSchema);
 
 module.exports = {
     PetModel
