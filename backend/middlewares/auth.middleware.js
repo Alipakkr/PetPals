d399c225fdc74c6b386b459d7dc6ee6d8ebf4769
imlnr <imlnr2002@gmail.com>
2024-02-21 19:41:43 +0530

    Final commit

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-const auth=(req,res,next)=>{
-    const token=req.headers.authorization?.split(" ")[1]
-    if(token){
-        const decoded=jwt.verify(token,"masai")
-        if(decoded){
-            req.body.userID=decoded.userID
-            req.body.author=decoded.author
+const auth = (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1]
+    if (token) {
+        const decoded = jwt.verify(token, "masai")
+        if (decoded) {
+            req.body.userID = decoded.userID
+            req.body.author = decoded.author
             next()
         } else {
-            res.send({"msg":"you are not authorised!"})
+            res.send({ "msg": "you are not authorised!" })
         }
     } else {
-        res.send({"msg":"you are not authorised!"})
+        res.send({ "msg": "you are not authorised!" })
     }
 }
 
-module.exports={
+module.exports = {
     auth
 }
