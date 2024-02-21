66033cd1fc18e74aa9d6b3ad89d58cf5649cea16
Alipa <alipakkr24@gmail.com>
2024-02-21 14:56:28 +0530

    backend

--- backend/models/pets.model.js
+++ backend/models/pets.model.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
-
 const petSchema = mongoose.Schema({
     petname:{type:String,required:true},
     breed:{type:String,required:true},
     gender:{type:String,required:true},
-    price:{type:Number,required:true}
+    price:{type:Number,required:true},
+    about:{type:String,required:true}
 },{
     versionKey:false
 })
-
 const PetModel = mongoose.model('pets',petSchema);
 
 module.exports = {
