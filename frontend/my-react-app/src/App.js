2cdf2ce6ac558c1e940e96334d310e73c4399305
imlnr <imlnr2002@gmail.com>
2024-02-22 20:48:03 +0530

    redux structure created

--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -1,19 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import MainRoutes from './AllRoutes/MainRoutes';
 import NavBar from './components/nvabar';
 import Home from './Pages/Home';
+import { store } from './Redux/store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MainRoutes/>
-        <NavBar/>
-        <Home/>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <MainRoutes />
+          <NavBar />
+          <Home />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
