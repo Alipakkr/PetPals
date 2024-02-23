4d48dae6f4b9d52202b8c3b6c7193e9427c2f396
Alipa <alipakkr24@gmail.com>
2024-02-23 14:06:40 +0530

    admin

--- frontend/my-react-app/src/AllRoutes/MainRoutes.jsx
+++ frontend/my-react-app/src/AllRoutes/MainRoutes.jsx
@@ -4,14 +4,16 @@ import Login from '../Pages/Login'
 import Signup from '../Pages/Signup'
 import Petadoption from '../Pages/Petadoption'
 import Home from '../Pages/Home'
+import Form from '../components/Form'
 
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/pet-adoption' element={<Petadoption/>}/>
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/pet-adoption' element={<Petadoption />} />
+    
     </Routes>
   )
 }
