11a158a1267acb88aaf5d3f1dbcc6d478333bc0e
imlnr <imlnr2002@gmail.com>
2024-02-24 18:39:41 +0530

    single page

--- frontend/my-react-app/src/Pages/Singlepet.jsx
+++ frontend/my-react-app/src/Pages/Singlepet.jsx
@@ -23,7 +23,15 @@ const Singlepet = () => {
                         <img src={state.image} alt="" />
                     </div>
                     <div className="pet-about">
-
+                        <h2>Details</h2>
+                        <p className='ind-name'><span>Name: </span>{state.name}</p>
+                        <p className='ind-breed'><span>Breed: </span>{state.breed}</p>
+                        <p className='ind-age'><span>Age: </span>{state.age}</p>
+                        <p className='ind-price'><span>Price: </span> {`$ ${state.price}`}</p>
+                        <p className='about-left span-cls'>About</p>
+                        <p className='about-left'>{`Introducing ${state.name}, a stunning ${state.breed} seeking a loving forever home. With a heart full of love, he/she adores human companionship and gets along famously with other pets. ${state.name}'s delightful personality shines through with his/her unique trait or behavior. Whether it's playtime in the park or cuddles on the couch, he/she is always up for it.`}</p>
+                        <p className='about-left'>{`We are reluctantly seeking a new home for ${state.name} due to unforeseen circumstances. We kindly request potential adopters to have the time, space, and resources to provide a loving environment. Adoption fees apply to ensure the best match. If you're ready to welcome a loyal and devoted companion into your life, reach out to us at Your Contact Information. Help ${state.name} find happiness in a forever home!`}</p>
+                        <button>{`Adopt ${state.name}`}</button>
                     </div>
                 </div>
             }
