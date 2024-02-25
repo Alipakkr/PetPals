2d1d5d7ab812c22ec85a442dc59767fe2b4ae0b4
imlnr <imlnr2002@gmail.com>
2024-02-26 02:32:17 +0530

    thank you added

--- frontend/my-react-app/src/Pages/Thankyou.jsx
+++ frontend/my-react-app/src/Pages/Thankyou.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import './styles/thankyou.css'
+import { useNavigate } from 'react-router-dom'
 
 const Thankyou = () => {
+    const navigate = useNavigate();
+    setTimeout(() => {
+        navigate('/')
+    }, 5000);
+
     return (
         <div id='thankyou'>
-            <div id="header">
-                <h1>THANK YOU!</h1>
+            <div id="tq-header">
+                <h1>Thank You!</h1>
             </div>
-
-            <div class="main-content">
-                <p>Thank you For adopting the pet.</p>
+            <div className='tick'><p>&#10004;</p></div>
+            <div className="main-content">
+                <p>Thanks a bunch for Adopting the pet. It means a lot to us, just like you do! We really appreciate you giving us a moment of your time today. Thanks for being you.</p>
             </div>
         </div>
     )
